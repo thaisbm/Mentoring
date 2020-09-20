a9563e0e3766f092d05fc65fe18a3dc12a02cf79
thaisbm <64555371+thaisbm@users.noreply.github.com>
2020-09-20 18:05:58 -0300

    Add files via upload

--- request.py
+++ request.py
@@ -1,18 +1,25 @@
-
+from typing import Dict
 
 import tweepy
 import requests
+import os
+from dotenv import load_dotenv
 
+with open('texto.txt', 'r') as tfile:
+    consumer_key = tfile.readline().strip('\n')
+    consumer_secret = tfile.readline().strip('\n')
+    access_token = tfile.readline().strip('\n')
+    access_token_secret = tfile.readline().strip('\n')
 
-auth = tweepy.OAuthHandler('CdxLDO29YQvSZWNBaTOqEt0rA', 'WzqGijOPtgjNpWRMbSizUgPIWVmSM9RZPtkfNh3KqUGHqbmo1u')
-auth.set_access_token('1295350845853315073-F9lV6uj8d3AVypCrkto6UQqgTrqFVj', 'iRYNLQrbiAvO51eqrlKyKqZpFbRNXKixNx4UdzYtFQ8ty')
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-payload = {'id': '1303338351290920960'}
-r = requests.get("https://twitter.com/Fernanda_Gentil", params=payload)
-r.text
 
-
-
-
-
+#load_dotenv('credential.txt')
+#token = os.getenv('TWEET TOKEN')
+token = 'AAAAAAAAAAAAAAAAAAAAAEpzGwEAAAAAA%2FhnALfeo3F%2BE%2FAvWwhJc4ZIdC0%3DhaMkykSFgVQZVzO4fmkehk903U5b5JSuZgq78RW3x5kMCTWttc'
+id = '1307786611095040000'
+headers = {'Authorization': f'Bearer {token}'}
+r = requests.get(url=f"https://api.twitter.com/2/tweets/{id}", headers=headers)
+print(r.text)
